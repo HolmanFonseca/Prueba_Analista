5a674df1449d21ee7ff5f50e9b2dcdf0853764b7
unknown <holmanfonseca@usantotomas.edu.co>
2022-02-21 10:59:11 -0500

    test

---
+++ test.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 21 10:52:03 2022
+
+@author: ASUS
+"""
+import nest_asyncio
+nest_asyncio.apply()
+
+from fastapi.testclient import TestClient
+
+from app import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Hello World"}
+
